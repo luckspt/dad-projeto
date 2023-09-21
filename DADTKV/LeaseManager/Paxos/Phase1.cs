406c488e52e7b2586a6e0336fc227c32f80982bf
Lucas Pinto <github@lucks.pt>
2023-09-21 17:44:09 +0100

    chore: improve on lease manager paxos structure

---
+++ DADTKV/LeaseManager/Paxos/Phase1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaseManager.Paxos
+{
+    struct Promises
+    {
+        public int greatestWriteEpoch;
+        public int receivedCount;
+    }
+
+    internal class Phase1
+    {
+        private Paxos paxos;
+        private Promises promises;
+
+        public Phase1(Paxos paxos)
+        {
+            this.paxos = paxos;
+            this.promises = new Promises { greatestWriteEpoch = 0, receivedCount = 0 };
+        }
+
+        public bool Prepare(int epoch)
+        {
+            // The acceptors only promise when the epoch is higher than the highest epoch they have seen
+            if (this.paxos.ReadTimestamp >= epoch)
+            {
+                return false;
+            }
+
+            this.paxos.ReadTimestamp = epoch;
+
+            // TODO: send promise(epoch, paxos.Value) back to proposer
+
+            return true;
+        }
+
+        public bool Promise(int epoch, List<Lease> leases)
+        {
+            this.promises.receivedCount++;
+
+            // We only care about the majority, so we can ignore the rest
+            if (this.promises.receivedCount >= this.paxos.peers.Count / 2)
+            {
+                // TODO: somewhere else (or here? :thinking:). If here, be careful with the >=
+                // 1. check if epoch=0.
+                // 1.a: If so, we propose our own value.
+                // 1.b: Otherwise, use the highest value we have seen.
+                // 2. send accept(epoch, value) to all acceptors
+                return false;
+            }
+
+            // If the epoch is higher than the highest epoch we have seen, update our values
+            if (epoch > this.promises.greatestWriteEpoch)
+            {
+                this.promises.greatestWriteEpoch = epoch;
+                this.paxos.Value = leases;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DADTKV/LeaseManager/Paxos/Phase2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaseManager.Paxos
+{
+    internal class Phase2
+    {
+        private Paxos paxos;
+        private int acceptedCount;
+
+        public Phase2 (Paxos paxos)
+        {
+            this.paxos = paxos;
+            this.acceptedCount = 0;
+        }
+
+        public bool Accept(int epoch, List<Lease> leases)
+        {
+            // We only accept if the epoch is the same as the one we have promised
+            if (this.paxos.ReadTimestamp != epoch)
+            {
+                return false;
+            }
+
+            this.paxos.Value = leases;
+            this.paxos.WriteTimestamp = epoch;
+            this.acceptedCount = 0;
+
+            // TODO: send accepted(epoch, paxos.Value or leases) back to proposer
+
+            return true;
+        }
+
+        public bool Accepted(int epoch, List<Lease> leases)
+        {
+            this.acceptedCount++;
+
+            // We only care about the majority, so we can ignore the rest
+            if (this.acceptedCount >= this.paxos.peers.Count / 2)
+            {
+                // We have consensus!!!
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
